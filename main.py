f243c48e8d875cd024c59da173022aa38e5e59c0
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-24 01:19:49 +0900

    auto commit

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 @bot.event
 async def on_ready():
     print(f"Bot is ready as {bot.user}")
-    activity = discord.Game(name="명령어: !help")
+    activity = discord.Activity(type=discord.ActivityType.custom, name="!help")
 
-    await bot.change_presence(status=discord.Status.online, activity=activity)
+    await bot.change_presence(activity=activity)
 
 @bot.command(name="help")
 async def help(ctx):
