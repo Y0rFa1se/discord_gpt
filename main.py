858374b52e5dae8ff797e20488824ff98420b4ff
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-23 15:46:03 +0900

    auto commit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ openai_init(ENV_DICT["GPT_API"])
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
