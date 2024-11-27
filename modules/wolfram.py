1f4d461995a60750ae6a35852360719a5a738d79
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-28 01:34:56 +0900

    auto commit

--- modules/wolfram.py
+++ modules/wolfram.py
@@ -1,8 +1,8 @@
 import wolframalpha
 
-def get_wolfram(query, app_id):
+async def get_wolfram(query, app_id):
     app_id = app_id
     client = wolframalpha.Client(app_id)
-    res = client.query(query)
+    res = await client.query(query)
 
     return next(res.results).text
