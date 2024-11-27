f7e879713357ba3e82a83806ec41b07df6388c3b
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-28 01:48:23 +0900

    auto commit

--- modules/wolfram.py
+++ modules/wolfram.py
@@ -3,6 +3,10 @@ import asyncio
 import matplotlib.pyplot as plt
 import os
 
+import requests
+from PIL import Image
+from io import BytesIO
+
 os.makedirs("files", exist_ok=True)
 
 async def get_wolfram(query, app_id):
@@ -11,9 +15,10 @@ async def get_wolfram(query, app_id):
     res = await asyncio.to_thread(client.query, query)
     res = next(res.results).text
 
-    plt.text(0.5, 0.5, res, ha='center', va='center')
-    plt.axis('off')
-    plt.savefig("files/wolfram.png")
-    plt.close()
+    image_url = next(res.pods).subpods[0].img['src']
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+
+    img.save("files/wolfram.png")
 
     return res
