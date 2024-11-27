29f12f0ae4a7e578d0fd44933c46750e0603a425
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-28 01:52:52 +0900

    auto commit

--- modules/wolfram.py
+++ modules/wolfram.py
@@ -9,20 +9,21 @@ from io import BytesIO
 os.makedirs("files", exist_ok=True)
 
 async def get_wolfram(query, app_id):
-    app_id = app_id
     client = wolframalpha.Client(app_id)
     res = await asyncio.to_thread(client.query, query)
 
     result_text = None
+    image_url = None
+
     for pod in res.pods:
         if pod.title == "Result":
             result_text = pod.subpods[0].plaintext if pod.subpods else "No text result"
-            break
-    
-    image_url = next(res.pods).subpods[0].img['src']
-    response = requests.get(image_url)
-    img = Image.open(BytesIO(response.content))
+        if pod.title == "Image" and pod.subpods:
+            image_url = pod.subpods[0].img['src']
 
-    img.save("files/wolfram.png")
+    if image_url:
+        response = requests.get(image_url)
+        img = Image.open(BytesIO(response.content))
+        img.save("files/wolfram.png")
 
     return result_text
