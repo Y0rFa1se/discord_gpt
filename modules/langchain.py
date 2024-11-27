f790a76b22ce4bbd260fa0791e41b0eeaa65bdb5
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-28 02:26:23 +0900

    auto commit

--- modules/langchain.py
+++ modules/langchain.py
@@ -2,4 +2,8 @@ from langchain.document_loaders import PyPDFLoader
 
 def process_pdf(pdf_binary):
     loader = PyPDFLoader(pdf_binary)
-    return loader.get_text()
+    text = loader.get_text()
+
+    print(text)
+
+    return text
