a3813c751b221e0083150ed2c14f73e49698d7e7
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-27 15:22:12 +0900

    auto commit

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ async def on_message(message):
         collected = ""
 
         for chunk in responses:
-            print(chunk.choices[0])
+            print(chunk.choices[0].delta)
             # collected += chunk.choices[0].message.content
             # collected += chunk['choices'][0]['delta']
 
