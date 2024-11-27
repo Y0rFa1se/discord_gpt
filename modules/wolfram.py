cf8600bc49d5e3ef432ccc444544e894b41462bd
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-28 01:50:28 +0900

    auto commit

--- modules/wolfram.py
+++ modules/wolfram.py
@@ -13,12 +13,21 @@ async def get_wolfram(query, app_id):
     app_id = app_id
     client = wolframalpha.Client(app_id)
     res = await asyncio.to_thread(client.query, query)
-    res = next(res.results).text
 
-    image_url = next(res.pods).subpods[0].img['src']
-    response = requests.get(image_url)
-    img = Image.open(BytesIO(response.content))
+    pod = next(res.pods, None)
+    if pod is None:
+        return "No results found"
+    
+    result_text = pod.subpods[0].plaintext if pod.subpods else "No text result"
+    
+    image_pod = next((p for p in res.pods if p.title == "Plot" or p.title == "Image"), None)
+    if image_pod and image_pod.subpods:
+        image_url = image_pod.subpods[0].img['src']
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image_url) as response:
+                img_data = await response.read()
+                img = Image.open(BytesIO(img_data))
+                img.save("files/wolfram.png")
 
-    img.save("files/wolfram.png")
-
-    return res
+    return result_text
