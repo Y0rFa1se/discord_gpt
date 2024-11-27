465ec44c429cb7ae7b7304f6bda84810c5bca681
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-28 02:32:39 +0900

    auto commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from io import BytesIO
 import asyncio
+import aiohttp
 
 from modules.openai import openai_init
 from modules.json import load_json, save_json
@@ -117,13 +118,22 @@ async def on_message(message):
                 await message.channel.send("Image uploaded.")
 
             elif attachment.content_type and attachment.content_type.startswith("pdf"):
-                file_data = await attachment.read()
-                text = "pdf content: "
-                text += process_pdf(BytesIO(file_data))
+                url = attachment.url
+
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as response:
+                        pdf_binary = await response.read()
+
+                        with open("files/pdf.pdf", "wb") as f:
+                            f.write(pdf_binary)
+
+                text = "pdf content: " + process_pdf(pdf_binary)
                 history = load_json(f"{message.guild}/{message.channel.category}", message.channel)
                 history = render_requests(history, text)
                 history = cut_message(history)
-                responses = gpt_request(history, MODEL)
+                save_json(f"{message.guild}/{message.channel.category}", message.channel, history)
+
+                await message.channel.send("PDF uploaded.")
 
     if message.content:
         requests = message.content
