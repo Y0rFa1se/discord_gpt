52b6c6974bb21408c6328c094db49faf2697a9a1
y0rfa1se0 <47736893+Y0rFa1se@users.noreply.github.com>
2024-11-28 02:00:26 +0900

    auto commit

--- modules/wolfram.py
+++ modules/wolfram.py
@@ -12,6 +12,8 @@ os.makedirs("files", exist_ok=True)
 async def get_wolfram(query, app_id):
     client = wolframalpha.Client(app_id)
     res = await asyncio.to_thread(client.query, query)
-    print(json.dumps(res, indent=4))
 
-    return result_text
+    with open("files/result.json", "w") as f:
+        json.dump(res, f, indent=4)
+
+    # return result_text
